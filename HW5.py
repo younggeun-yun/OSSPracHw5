d79bd58c11004dbd08b3eb26af7e2634e669c6e7
younggeun-yun <yyg1006@dongguk.edu>
2023-10-08 17:53:26 +0900

    ossp2 branch

--- HW5.py
+++ HW5.py
@@ -1,6 +1,8 @@
 name=input("이름을 입력하세요: ")
 number=input("학번을 입력하세요: ")
 name2=input("학과를 입력하세요: ")
+number2=input("학년을 입력하세요: ")
 print("\n<출력>\n이름은: "+name)
 print("학번은: "+number)
 print("학과는: "+name2)
+print("학년은: "+number2)
